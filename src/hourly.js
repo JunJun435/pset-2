7a868efbee158eb2655af1de93c3dfe25666df2e
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-10-26 20:09:38 -0400

    fixed mistake

--- src/hourly.js
+++ src/hourly.js
@@ -7,6 +7,6 @@ const th=Number(scanner.question("Thursday: "));
 const f=Number(scanner.question("Friday: "));
 const s=Number(scanner.question("Saturday: "));
 const su=Number(scanner.question("Sunday: "));
-let wage=(m*hourlyWage)+(t*hourlyWage)+(w*hourlyWage)+(th*hourlyWage)+(f*hourlyWage)+(s*hourlyWage)+(su*hourlyWage);
+let wage=(m*hourlyWage) + (t*hourlyWage) + (w*hourlyWage) + (th*hourlyWage) + (f*hourlyWage) + (s*hourlyWage) + (su*hourlyWage);
 total=wage.toLocaleString('en',{ style:'currency', currency: 'USD'})
 console.log("\nYou'll make " + total + " this week.");
